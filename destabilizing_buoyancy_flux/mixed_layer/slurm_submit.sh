95b3a7fabba5ac3753d5f016abc69a314019bf6c
glwagner <wagner.greg@gmail.com>
2019-02-14 14:29:31 -0500

    Adds slurm submit script

--- destabilizing_buoyancy_flux/mixed_layer/slurm_submit.sh
+++ destabilizing_buoyancy_flux/mixed_layer/slurm_submit.sh
@@ -7,29 +7,36 @@
 # sched_any_quicktest   2       2-8-1   00:15:00
 # newnodes              (32)    2-10-1  12:00:00
 
-nodes=$1
+if [ $1 -eq 0 ]; then
+    nodes=1
+else
+    nodes=$1
+fi
+
 jobname="ML$nodes"
-closure="StratifiedAnisotropicMinimumDissipation"
+closure="ConstantSmagorinsky" 
+#closure="StratifiedAnisotropicMinimumDissipation"
 scriptname="$jobname.slurm"
+walltime="4:00:00"
 
 rm -f $scriptname
 
 echo "#!/bin/bash
 
 # Job
-##SBATCH --reservation=wagner
+#SBATCH --reservation=wagner
 #SBATCH --partition=sched_mit_hill
-##SBATCH --qos=plenum
+###SBATCH --partition=sched_mit_raffaele
+#SBATCH --qos=plenum
 
 #SBATCH --nodes=$nodes
-#SBATCH --ntasks-per-node=16
+#SBATCH --ntasks-per-node=20
 #SBATCH --mem-per-cpu=4000
-#SBATCH --time=4:00:00
+#SBATCH --time=$walltime
 #SBATCH --job-name=$jobname
 
 # Streams
-#SBATCH --output=job_%j.out
-#SBATCH --error=job_%j.err
+#SBATCH --output=$jobname_%j.out
 
 # Activate conda and dedalus environment
 export DEDALES='/nobackup1/glwagner/dedaLES'
@@ -40,3 +47,4 @@ conda activate dedalus
 mpiexec python3 mixed_layer_convection.py $closure >> $jobname.out" >> $scriptname
 
 sbatch $scriptname
+squeue -u $USER
